b4b53d85b7a231c533ec2904ce4eb2788260ace9
Myrraz <pelusa_dic2011@hotmail.com>
2024-11-05 21:01:27 -0500

    final

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+    $conexion = new mysqli("localhost", "root", "", "clase3");//creamos variable para la conexion a la base de datos
+    /** necesitamos 
+     * servidor = localhost
+     * usuario = root
+     * pasword = null
+     * nombre base de datos = clase3
+     */
+
+     var_dump($conexion)//
+?>
--- index.php
+++ index.php
@@ -1,12 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Adios<h1>
-    
+    <form action="" method="get">
+        <select name="desplegable" id="">
+            <option value="1">Opcion1</option>
+            <option value="2">Opcion2</option>
+        </select>
+        <button type="submit">Enviar</button>
+
+        <table border="1">
+            <thead>
+                <th>Documentos</th>
+                <th>Nombres</th>
+                <th>Telefono</th>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>a</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>b</td>
+                    <td>22</td>
+                </tr>
+
+                <?php
+
+                $inicio = 1;
+
+                while ($inicio <= 10) {//generando mas filas 10 mas
+                    $inicio++;
+
+                ?>
+                    <tr>
+                        <td>2</td>
+                        <td>b</td>
+                        <td>22</td>
+                    </tr>
+
+                <?php
+
+                }
+
+                ?>
+
+            </tbody>
+
+        </table>
+
+    </form>
 </body>
+
 </html>
